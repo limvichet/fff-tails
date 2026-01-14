ac3e0ef6486827a984d94c42f20dcbe4f2860092
limvichet <83205736+limvichet@users.noreply.github.com>
2026-01-14 19:26:27 +0700

    fix nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,7 +52,8 @@ export default defineNuxtConfig({
     preset: 'github-pages',
     // បើចង់ប្រើ SSR: true ត្រូវថែម prerender ឱ្យគ្រប់ route
     prerender: {
-      routes: ['/', '/dashboard']
+      routes: ['/', '/app/dashboard'],
+      failOnError: false, 
     }
   },
 })
