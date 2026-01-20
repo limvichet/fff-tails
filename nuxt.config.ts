6e7440e54327e11c765359812650a6cf90a1c1e3
limvichet <83205736+limvichet@users.noreply.github.com>
2026-01-20 15:49:55 +0700

    fix bashURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   future: { compatibilityVersion: 4},
   css: ['@/assets/css/main.css'],
-  vite: { plugins: [tailwindcss()] },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/image', "@nuxtjs/color-mode", 'nuxt-og-image'],
   site: {
     url: 'https://limvichet.github.io/fff-tails',
   },
-  app: {
-    baseURL: process.env.NODE_ENV === 'production' ? '/fff-tails/' : '/',
-  },
+  // app: {
+  //   baseURL: process.env.NODE_ENV === 'production' ? '/fff-tails/' : '/',
+  // },
   colorMode: {
     storageKey: 'fff-color-theme',
     fallback: 'light',
