72fbe82d489e9854a57cd77503dc54acaea117bf
limvichet <83205736+limvichet@users.noreply.github.com>
2026-03-21 18:21:57 +0700

    fix Notification color

--- app/components/layout/header/NotificationMenu.vue
+++ app/components/layout/header/NotificationMenu.vue
@@ -112,8 +112,7 @@ const editLoan = (id: number) => {
     <!-- Notification Bell -->
     <button
       class="relative flex items-center justify-center text-gray-500 transition-colors bg-white border border-gray-200 rounded-full hover:text-dark-900 h-11 w-11 hover:bg-gray-100 hover:text-gray-700"
-      @click="toggleDropdown"
-    >
+      @click="toggleDropdown">
       <!-- Unread dot -->
       <!-- <span
         :class="{ hidden: !notifying, flex: notifying }"
@@ -124,18 +123,18 @@ const editLoan = (id: number) => {
       <!-- <span v-if="unreadCount > 0" class="absolute right-0 top-0.5 h-2 w-2 rounded-full bg-orange-400">
         <span class="absolute w-full h-full bg-orange-400 rounded-full opacity-75 animate-ping"></span>
       </span> -->
-            <!-- Unread badge -->
-      <span v-if="unreadCount > 0" 
-            class="absolute -top-1 -right-1 flex items-center justify-center h-5 w-5 rounded-full bg-orange-400 text-white text-xs">
+      <!-- Unread badge -->
+      <span v-if="unreadCount > 0"
+        class="absolute -top-1 -right-1 flex items-center justify-center h-5 w-5 rounded-full bg-orange-400 text-white text-xs">
         {{ unreadCount }}<span class="absolute w-4 h-4 bg-orange-400 rounded-full opacity-75 animate-ping"></span>
       </span>
 
 
       <!-- Bell Icon -->
-        <svg class="fill-current" width="20" height="20" viewBox="0 0 20 20">
-          <path fill-rule="evenodd" clip-rule="evenodd"
-            d="M10.75 2.29248C10.75 1.87827 10.4143 1.54248 10 1.54248C9.58583 1.54248 9.25004 1.87827 9.25004 2.29248V2.83613C6.08266 3.20733 3.62504 5.9004 3.62504 9.16748V14.4591H3.33337C2.91916 14.4591 2.58337 14.7949 2.58337 15.2091C2.58337 15.6234 2.91916 15.9591 3.33337 15.9591H4.37504H15.625H16.6667C17.0809 15.9591 17.4167 15.6234 17.4167 15.2091C17.4167 14.7949 17.0809 14.4591 16.6667 14.4591H16.375V9.16748C16.375 5.9004 13.9174 3.20733 10.75 2.83613V2.29248ZM14.875 14.4591V9.16748C14.875 6.47509 12.6924 4.29248 10 4.29248C7.30765 4.29248 5.12504 6.47509 5.12504 9.16748V14.4591H14.875ZM8.00004 17.7085C8.00004 18.1228 8.33583 18.4585 8.75004 18.4585H11.25C11.6643 18.4585 12 18.1228 12 17.7085C12 17.2943 11.6643 16.9585 11.25 16.9585H8.75004C8.33583 16.9585 8.00004 17.2943 8.00004 17.7085Z" />
-        </svg>
+      <svg class="fill-current" width="20" height="20" viewBox="0 0 20 20">
+        <path fill-rule="evenodd" clip-rule="evenodd"
+          d="M10.75 2.29248C10.75 1.87827 10.4143 1.54248 10 1.54248C9.58583 1.54248 9.25004 1.87827 9.25004 2.29248V2.83613C6.08266 3.20733 3.62504 5.9004 3.62504 9.16748V14.4591H3.33337C2.91916 14.4591 2.58337 14.7949 2.58337 15.2091C2.58337 15.6234 2.91916 15.9591 3.33337 15.9591H4.37504H15.625H16.6667C17.0809 15.9591 17.4167 15.6234 17.4167 15.2091C17.4167 14.7949 17.0809 14.4591 16.6667 14.4591H16.375V9.16748C16.375 5.9004 13.9174 3.20733 10.75 2.83613V2.29248ZM14.875 14.4591V9.16748C14.875 6.47509 12.6924 4.29248 10 4.29248C7.30765 4.29248 5.12504 6.47509 5.12504 9.16748V14.4591H14.875ZM8.00004 17.7085C8.00004 18.1228 8.33583 18.4585 8.75004 18.4585H11.25C11.6643 18.4585 12 18.1228 12 17.7085C12 17.2943 11.6643 16.9585 11.25 16.9585H8.75004C8.33583 16.9585 8.00004 17.2943 8.00004 17.7085Z" />
+      </svg>
     </button>
 
     <!-- Dropdown -->
@@ -143,31 +142,54 @@ const editLoan = (id: number) => {
       v-if="dropdownOpen"
       class="absolute right-0 mt-2 flex h-auto w-[350px] flex-col rounded-2xl border bg-white p-3 shadow-lg overflow-y-auto"
     > -->
-    <div
-      v-if="dropdownOpen"
-    class="absolute mt-2 flex h-auto w-[350px] flex-col rounded-2xl border bg-white p-3 shadow-lg overflow-y-auto
-           left-0 lg:right-0 lg:left-auto"
-    >
-      <h5 class="text-lg font-semibold mb-3">Loan Notifications ({{ unreadCount }})</h5>
+    <div v-if="dropdownOpen" class="absolute mt-2 flex w-[350px] max-h-[400px] flex-col rounded-2xl 
+         border border-gray-200 dark:border-gray-700
+         bg-white dark:bg-gray-900
+         p-3 shadow-lg overflow-y-auto
+         left-0 lg:right-0 lg:left-auto z-50">
+      <!-- Header -->
+      <h5 class="text-lg font-semibold mb-3 text-gray-800 dark:text-gray-200">
+        Loan Notifications ({{ unreadCount }})
+      </h5>
 
-      <ul>
-        <li
-          v-for="loan in notifications"
-          :key="loan.id"
-          class="p-3 border-b hover:bg-gray-100 cursor-pointer"
-          @click="handleItemClick($event, loan)"
-        >
-          #{{ loan.id }} - Total: {{ loan.loan_totalcash }} 
-          <span v-if="loan.loan_check_status === 0" class="text-blue-800 ml-2">(Unread)</span>
+      <!-- List -->
+      <ul class="divide-y divide-gray-200 dark:divide-gray-700">
+        <li v-for="loan in notifications" :key="loan.id" @click="handleItemClick($event, loan)" :class="[
+          'p-3 cursor-pointer transition rounded-lg',
+          'text-gray-800 dark:text-gray-200',
+          'hover:bg-gray-100 dark:hover:bg-gray-800',
+          loan.loan_check_status === 0
+            ? 'bg-blue-50 dark:bg-blue-900/20'
+            : ''
+        ]">
+          <div class="flex justify-between items-center">
+            <span>
+              #{{ loan.id }} - {{ loan.loan_totalcash }}
+            </span>
+
+            <span v-if="loan.loan_check_status === 0" class="text-xs font-medium text-blue-600 dark:text-blue-400">
+              Unread
+            </span>
+          </div>
         </li>
       </ul>
 
-      <button
-        class="mt-3 p-2 border rounded w-full hover:bg-gray-100"
-        @click="handleViewAllClick"
-      >
-        View All Loans
-      </button>
+      <!-- Empty state -->
+      <div v-if="notifications.length === 0" class="text-center py-4 text-gray-500 dark:text-gray-400">
+        No notifications
+      </div>
+
+      <!-- Button -->
+      <!-- <button
+    class="mt-3 p-2 rounded-lg w-full border 
+           border-gray-200 dark:border-gray-700
+           text-gray-700 dark:text-gray-200
+           hover:bg-gray-100 dark:hover:bg-gray-800
+           transition"
+    @click="handleViewAllClick"
+  >
+    View All Loans
+  </button> -->
     </div>
   </div>
 </template>
