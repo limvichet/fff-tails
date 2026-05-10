4cd65fda009ae24ee23df04e45dced359ec1c894
limvichet <83205736+limvichet@users.noreply.github.com>
2026-05-10 09:21:01 +0000

    Deploying to gh-pages from @ limvichet/fff-tails@810c86fc631019e8b5fec6ff36335952590e3b85 🚀

---
+++ _nuxt/-0Khv11S.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DZEu6la-.js";import{_ as n,c as r,o as a,b as _,w as c,r as s}from"./D1OPQaTp.js";const l={};function d(e,f){const t=o;return a(),r("div",null,[_(t,null,{default:c(()=>[s(e.$slots,"default")]),_:3})])}const i=n(l,[["render",d]]);export{i as default};
--- _nuxt/TtqUvGIO.js
+++
@@ -1,1 +0,0 @@
-import{p as s}from"./CoeAE80_.js";const p=s("/imgs/logo.png");export{p as _};
