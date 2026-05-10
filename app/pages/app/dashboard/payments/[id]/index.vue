810c86fc631019e8b5fec6ff36335952590e3b85
limvichet <83205736+limvichet@users.noreply.github.com>
2026-05-10 16:20:15 +0700

    fix button payment cashin submit

--- app/pages/app/dashboard/payments/[id]/index.vue
+++ app/pages/app/dashboard/payments/[id]/index.vue
@@ -562,7 +562,7 @@ const getStatusClass = (s: Schedule) => {
               name="svg-spinners:180-ring-with-bg" 
               class="text-lg" 
             />
-            <span v-if="paymentCashinLoading">....</span>
+            <span v-if="paymentCashinLoading">Wait</span>
             <span v-else>{{ paymentCashinIsEditMode ? "Update" : "Save" }}</span>
           </button>
 
